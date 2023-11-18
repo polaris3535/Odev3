c5e2125b33d1d1768fe6b068e60f63a50c338532
polaris3535 <149302151+polaris3535@users.noreply.github.com>
2023-11-18 17:36:50 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,134 +1,44 @@
 import "./App.css";
-import React from 'react';
-
-
-
-function Arama({aramaMetni, onSearch, }){
- 
-  function handleChange(event){
-    setAramaMetni(event.target.value)
-     props.onSearch(event);
-     localStorage.setItem("aranan", event.target.value); 
-  }
-  React.useEffect(() => {
-    localStorage.setItem("aranan", aramaMetni); 
-  },[aramaMetni]);
-    return(
-    <div>     
-      <label htmlFor="arama">Ara: </label>
-      <input id="arama" type="text" onChange={onSearch} value={aramaMetni}   />  
-      <p>
-        
-      </p>
-      </div>
-  )
-}
-
-
-
-function Yazi({id, url, baslik, yazar, yorum_sayisi, puan}){
-  return(
-    <li key={id}>
-          <span>
-            <a href={url}>{baslik}</a>, 
-          </span>
-          <span><b>Yazar:</b> {yazar}, </span>
-          <span><b>Yorum Sayısı:</b> {yorum_sayisi}, </span>
-          <span><b>Puan:</b> {puan}</span>
-        </li>
-  )
-}
-
-
-
-
-
-function Liste(props) {
-  return(
-  <ul>
-   {props.yazilar.map(function (yazi){
-    return (
-    
-    <Yazi key={yazi.id} {...yazi}/>
-    );
-   })}
-
-  </ul>
-
-  )                     
-}
-
-
-
-
-
+const yaziListesi = [
+  {
+    baslik: "React Öğreniyorum",
+    url: "www.sdu.edu.tr",
+    yazar: "Sinan Yüksel",
+    yorum_sayisi: 3,
+    puan: 4,
+    id: 0,
+  },
+  {
+    baslik: "Web Teknolojileri ve Programlama",
+    url: "wwww.google.com.tr",
+    yazar: "Asım Yüksel",
+    yorum_sayisi: 2,
+    puan: 5,
+    id: 1,
+  },
+];
 function App() {
-  const [aramaMetni, setAramaMetni] = React.useState(localStorage.getItem("aranan") ||  "React");
-
-  const yaziListesi = [
-    {  
-      baslik: "React Öğreniyorum",
-      url: "www.sdu.edu.tr",
-      yazar: "Sinan Yüksel",
-      yorum_sayisi: 3,
-      puan: 4,
-      id: 2,
-    },
-    {
-      baslik: "Web Teknolojileri ve Programlama",
-      url: "wwww.google.com.tr",
-      yazar: "Asım Yüksel",
-      yorum_sayisi: 2,
-      puan: 5,
-      id: 1,
-    },
-
-    {
-      baslik: "Unity ile Mobil Oyun",
-      url: "https://veyselkinaci.vercel.app",
-      yazar: "Veysel Kinaci",
-      yorum_sayisi: 556,
-      puan: 5.0,
-      id: 0
-    },
-
-    {
-      baslik: "Unity nedir? Unity'nin Avantajları",
-      url: "www.canakandemir.com/",
-      yazar: "Furkan Atici",
-      yorum_sayisi: 12,
-      puan: 4.7,
-      id: 3
-    },
-    
-  ];
-
-  const arananYazilar = yaziListesi.filter(function (yazi) {
-    return (
-      yazi.baslik.toLowerCase().includes(aramaMetni.toLowerCase()) || yazi.yazar.toLowerCase().includes(aramaMetni.toLowerCase()) 
-    );
-  });
-
-
-
-
-  function handleSearch(event){
-   setAramaMetni(event.target.value);
-  }
-
-  React.useEffect(()=>{
-    localStorage.setItem("aranan",aramaMetni);
-  },[aramaMetni]);
-
   return (
     <div>
       <h1>Yazılar</h1>
-      <Arama aramaMetni={aramaMetni}  onSearch = {handleSearch} />  
-      <strong>{aramaMetni} araniyor...</strong>
+      <label htmlFor="arama">Ara: </label>
+      <input id="arama" type="text" />
       <hr />
-      <Liste yazilar={arananYazilar}/>
+      <ul>
+        {yaziListesi.map(function (yazi) {
+          return (
+            <li key={yazi.id}>
+              <span>
+                <a href={yazi.url}>{yazi.baslik}</a>, 
+              </span>
+              <span><b>Yazar:</b> {yazi.yazar}, </span>
+              <span><b>Yorum Sayısı:</b> {yazi.yorum_sayisi}, </span>
+              <span><b>Puan:</b> {yazi.puan}</span>
+            </li>
+          );
+        })}{" "}
+      </ul>
     </div>
   );
-
 }
-export default App;
+export default App;
